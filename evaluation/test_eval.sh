8a823ce6cbd8a854476933ce8771311b05f96166
Anlon Zhu <zhu.anlon@gmail.com>
2025-05-06 00:43:42 -0400

    test

---
+++ evaluation/test_eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set required environment variables
+export HF_ALLOW_CODE_EVAL=1
+export HF_DATASETS_TRUST_REMOTE_CODE=true
+export LOGLEVEL=INFO
+
+# Run with minimal settings for testing
+accelerate launch \
+    eval_llada.py \
+    --tasks humaneval \
+    --model llada_dist \
+    --confirm_run_unsafe_code \
+    --model_args model_path='GSAI-ML/LLaDA-8B-Base',gen_length=32,steps=8,block_length=32,num_samples=1 \
+    --num_fewshot=0 \
+    --limit=2
