9e389f7d02fb2ccb1a8cecee8d8c417d1d6adb48
Anlon Zhu <zhu.anlon@gmail.com>
2025-05-07 16:31:58 -0400

    update

--- evaluation/runall.sh
+++ evaluation/runall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # sbatch mbpp.sbatch
-# sbatch mbpp_instruct.sbatch
+sbatch mbpp_instruct.sbatch
 # sbatch humaneval.sbatch
-# sbatch humaneval_instruct.sbatch
+sbatch humaneval_instruct.sbatch
 sbatch codexglue_java.sbatch
 sbatch codexglue_go.sbatch
 sbatch codexglue_javascript.sbatch
